11af4374b6cffd3589edd4e207b8be9df23a6aa6
Vishal6283dk <vishal25goyal25@gmail.com>
2025-06-03 18:05:38 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from networksecurity.components.data_ingestion import DataIngestion
 
+from networksecurity.components.data_validation import DataValidation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 
@@ -13,16 +15,25 @@ import sys
 if __name__=='__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
-        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion_config=DataIngestionConfig(trainingpipelineconfig)
 
-        data_ingestion=DataIngestion(dataingestionconfig)
-
+        data_ingestion=DataIngestion(data_ingestion_config)
         logging.info("Initiate the data ingestion")
 
-        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
+        print(data_ingestion_artifact)
+
+
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
         
-        print(dataingestionartifact)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+        
 
 
     except Exception as e:
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -28,3 +28,13 @@ SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 SAVED_MODEL_DIR =os.path.join("saved_models")
 MODEL_FILE_NAME = "model.pkl"
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
